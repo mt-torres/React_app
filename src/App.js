8b6f0c1c07bf8f088d53d5c78bf7eb96a124fd3c
mt-torres <mf_torres@yahoo.com.br>
2021-10-13 17:42:26 -0300

    create class ArrayNota and categorias

--- src/App.js
+++ src/App.js
@@ -5,57 +5,37 @@ import '../src/assets/css/reset.css';
 import '../src/assets/css/style.css';
 import '../src/assets/css/index.css';
 import ListaDeCategorias from './components/ListaDeCategorias/ListaDeCategorias';
+import Categorias from './dados/Categorias.js';
+import ArrayNotas from './dados/ArrayNotas.js';
 
 class App extends Component {
 
   constructor(){
     super();
-    
-    this.state = {
-      notas: [],
-      categorias: [],
-    };
+    this.categorias = new Categorias();
+    this.notas = new ArrayNotas();
   }
 
-  criarNota(titulo, texto, categoria){
-    const novaNota = {titulo, texto, categoria};
-    const novoArrayNotas = [...this.state.notas,novaNota];
-    const novoEstado = {
-      notas:novoArrayNotas
-    }
-    this.setState(novoEstado)
-  }
 
-  criaCategoria(nomeCategoria){
-    const novoArrayCategorias = [...this.state.categorias,nomeCategoria];
-    const novoEstado = {...this.state, categorias: novoArrayCategorias }
-    this.setState(novoEstado)
-  }
-
-  deletaNota(index){
-    let arrayNotas = this.state.notas;
-    arrayNotas.splice(index,1);
-    this.setState({notas:arrayNotas});
-  }
 
   render(){
     return (
       <section className="main">
         <div className="main__formulario-cadastro">
           <FormularioCadastro 
-            criarNota={this.criarNota.bind(this)}
-            categorias={this.state.categorias}
+            categorias={this.categorias.categorias}
+            criarNota={this.notas.adicionarNota}
           />
         </div>
         <div className="main__lista-notas">
           <ListaDeCategorias
-            adicionarCategoria= {this.criaCategoria.bind(this)}
-            categorias={this.state.categorias}
+            categorias={this.categorias.categorias}
+            adicionarCategoria= {this.categorias.adicionarCategoria}
 
             />
           <ListaDeNotas 
-            apagaNota= {this.deletaNota.bind(this)}
-            notas ={this.state.notas}
+            apagaNota= {this.notas.apagarNota}
+            notas ={this.notas.notas}
           />
         </div>
       </section>
---
+++ src/dados/Categorias.js
@@ -0,0 +1,14 @@
+export default class Categorias{
+
+    constructor(){
+        this.categorias = [];
+    }
+
+    adicionarCategoria(novaCategoria){
+      
+        this.categorias.push(novaCategoria);
+      
+    }
+
+
+}
